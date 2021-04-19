b02ebdd73233baeffb0b6d5f09255e964c9e4d1a
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-19 16:49:05 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -9,7 +9,7 @@ function AppHeader() {
       <div className="mdc-top-app-bar__row">
         <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
           <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button" aria-label="Open navigation menu">menu</button>
-          <span className="mdc-top-app-bar__title">Page title</span>
+          <span className="mdc-top-app-bar__title">Inicio</span>
         </section>
         <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
           <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Favorite">favorite</button>
