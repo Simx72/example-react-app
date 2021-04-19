6aee5a344cc11e4cb9c9ddb8bac575eb6af1521b
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-19 11:43:07 -0500

    add to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { AppBody } from './App/body'
 import 'material-design-icons/iconfont/material-icons.css';
+import { AppHeader } from './App/header';
 
 function App() {
   return (
     <div id="App">
+      <AppHeader />
       <AppBody />
     </div>
   );
