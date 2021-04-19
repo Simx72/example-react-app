d4492c3f32dbd14a555ad8525f510282dffeabba
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-19 11:52:13 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -2,6 +2,23 @@ import "@material/drawer/dist/mdc.drawer.min.css";
 import "@material/list/dist/mdc.list.min.css";
 
 function AppHeader() {
+
+  let topappbar = (
+    <header className="mdc-top-app-bar">
+      <div className="mdc-top-app-bar__row">
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button" aria-label="Open navigation menu">menu</button>
+          <span className="mdc-top-app-bar__title">Page title</span>
+        </section>
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Favorite">favorite</button>
+          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Search">search</button>
+          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Options">more_vert</button>
+        </section>
+      </div>
+    </header>
+  )
+
   let drawer = (
     <aside className="mdc-drawer">
       <div className="mdc-drawer__content">
