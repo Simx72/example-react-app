95887169dfc7fac78c931bac9f38c78ec9f7675c
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 17:33:31 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,8 +1,7 @@
 import TopAppBar, { TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
 
-const AppHeader = (a: any, b: any, c: any) => {
-  console.log(a, b, c)
+const AppHeader = (props: { title: string }) => {
   return (
     <TopAppBar>
       <TopAppBarRow>
