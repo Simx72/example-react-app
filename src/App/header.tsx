cad99209ee0589abee8b57cf792f16e2dd7b57fe
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 07:33:35 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,44 +1,44 @@
-import '@material/react-top-app-bar/index.scss';
-import '@material/react-material-icon/index.scss';
-import TopAppBar, {
-  TopAppBarFixedAdjust,
-  TopAppBarIcon,
-  TopAppBarRow,
-  TopAppBarSection,
-  TopAppBarTitle,
-} from '@material/react-top-app-bar';
+import { Component } from 'react';
+import TopAppBar, { TopAppBarFixedAdjust } from '@material/react-top-app-bar';
+import Drawer, { DrawerAppContent, DrawerContent, DrawerHeader, DrawerTitle } from '@material/react-drawer';
 import MaterialIcon from '@material/react-material-icon';
+import List, { ListItem, ListItemGraphic, ListItemText } from '@material/react-list';
 
-function AppHeader() {
+// includes imports for drawer, list, material icon and top app bar styles
+import './App.scss';
 
-  return (
-    <div>
-      <TopAppBar>
-        <TopAppBarRow>
-          <TopAppBarSection align='start'>
-            <TopAppBarIcon navIcon tabIndex={0}>
-              <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')} />
-            </TopAppBarIcon>
-            <TopAppBarTitle>Miami, FL</TopAppBarTitle>
-          </TopAppBarSection>
-          <TopAppBarSection align='end' role='toolbar'>
-            <TopAppBarIcon actionItem tabIndex={0}>
-              <MaterialIcon
-                aria-label="print page"
-                hasRipple
-                icon='print'
-                onClick={() => console.log('print')}
-              />
-            </TopAppBarIcon>
-          </TopAppBarSection>
-        </TopAppBarRow>
-      </TopAppBar>
-      <TopAppBarFixedAdjust>
-        My exciting content!
-      </TopAppBarFixedAdjust>
-    </div>
-  )
+export class AppHeader extends Component {
+  state = { selectedIndex: 0 };
 
+  render() {
+    return (
+      <div className='drawer-container'>
+        <Drawer>
+          <DrawerHeader>
+            <DrawerTitle tag='h2'>
+              jane.smith@gmail.com
+            </DrawerTitle>
+          </DrawerHeader>
+
+          <DrawerContent>
+            <List singleSelection selectedIndex={this.state.selectedIndex}>
+              <ListItem>
+                <ListItemGraphic graphic={<MaterialIcon icon='folder' />} />
+                <ListItemText primaryText='Mail' />
+              </ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent className='drawer-app-content'>
+          <TopAppBar
+            title='Inbox' />
+
+          <TopAppBarFixedAdjust>
+            Your inbox content
+          </TopAppBarFixedAdjust>
+        </DrawerAppContent>
+      </div>
+    );
+  }
 }
-
-export { AppHeader }
