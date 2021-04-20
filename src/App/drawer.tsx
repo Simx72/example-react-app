653ee15d42f24e837401f8953cd1b603b6b566ef
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 17:24:01 -0500

    add to project

---
+++ src/App/drawer.tsx
@@ -0,0 +1,72 @@
+import Drawer, { DrawerAppContent, DrawerContent, DrawerHeader, DrawerSubtitle, DrawerTitle } from "@material/react-drawer";
+import List, { ListItem, ListItemGraphic, ListItemText } from "@material/react-list";
+import MaterialIcon from "@material/react-material-icon";
+import { TopAppBarFixedAdjust } from "@material/react-top-app-bar";
+import { Component } from "react";
+
+export class AppHeader extends Component {
+  state = { selectedIndex: 0 };
+
+  componentDidMount() {
+    switch (window.location.pathname) {
+      case '/':
+        this.setState({
+          selectedIndex: 0
+        })
+        break;
+      case '/otro':
+        this.setState({
+          selectedIndex: 1
+        })
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  listEltClick(elt?: HTMLElement) {
+    if (typeof elt != 'undefined') {
+      let href = elt.getAttribute('href')
+      if (href != null) {
+        window.location.assign(href)
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className='drawer-container'>
+        <Drawer>
+          <DrawerHeader>
+            <DrawerTitle tag='h2'>
+              cualquier.correo@gmail.com
+            </DrawerTitle>
+            <DrawerSubtitle tag='em'>
+              @nombre_de_usuario
+            </DrawerSubtitle>
+          </DrawerHeader>
+
+          <DrawerContent>
+            <List singleSelection selectedIndex={this.state.selectedIndex}>
+              <ListItem href="/" onClick={(ev) => this.listEltClick(ev.target as HTMLElement)}>
+                <ListItemGraphic graphic={<MaterialIcon icon='home' />} />
+                <ListItemText primaryText="Inicio" />
+              </ListItem>
+              <ListItem href="/otro" onClick={(ev) => this.listEltClick(ev.target as HTMLElement)}>
+              <ListItemGraphic graphic={<MaterialIcon icon='favorite' />} />
+                Cualquier otra cosa
+              </ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+
+        <DrawerAppContent className='drawer-app-content'>
+          <TopAppBarFixedAdjust>
+            Your inbox content
+          </TopAppBarFixedAdjust>
+        </DrawerAppContent>
+      </div>
+    );
+  }
+}
