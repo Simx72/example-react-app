3d078e28df96f50706151f8c0f6a66357357ddf2
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 11:06:34 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import TopAppBar, { TopAppBarFixedAdjust, TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
-import Drawer, { DrawerAppContent, DrawerContent, DrawerHeader, DrawerTitle } from '@material/react-drawer';
 import MaterialIcon from '@material/react-material-icon';
-import List, { ListItem, ListItemGraphic, ListItemText } from '@material/react-list';
 
 import '@material/react-material-icon/index.scss';
 import '@material/react-top-app-bar/index.scss';
 import '@material/react-list/index.scss';
 import '@material/react-drawer/index.scss';
 
-const Navbar = () => {
+const AppHeader = () => {
   return (
     <div>
       <TopAppBar>
@@ -39,33 +36,4 @@ const Navbar = () => {
   );
 }
 
-export class AppHeader extends Component {
-  state = {selectedIndex: 0};
-
-  render() {
-    return (
-      <div className='drawer-container'>
-        <Drawer>
-          <DrawerHeader>
-            <DrawerTitle tag='h2'>
-              jane.smith@gmail.com
-            </DrawerTitle>
-          </DrawerHeader>
-
-          <DrawerContent>
-            <List singleSelection selectedIndex={this.state.selectedIndex}>
-              <ListItem>
-                <ListItemGraphic graphic={<MaterialIcon icon='folder'/>} />
-                <ListItemText primaryText='Mail' />
-              </ListItem>
-            </List>
-          </DrawerContent>
-        </Drawer>
-
-        <DrawerAppContent className='drawer-app-content'>
-          <Navbar />
-        </DrawerAppContent>
-      </div>
-    );
-  }
-}
+export { AppHeader }
