02a07e85ebfe6e77414326dfd99186fc717b2ada
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 07:12:20 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,20 +1,10 @@
+import MDCTopAppBarBaseFoundation from "@material/top-app-bar/foundation";
+
 function AppHeader() {
 
-  let topappbar = (
-    <header className="mdc-top-app-bar">
-      <div className="mdc-top-app-bar__row">
-        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-          <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button" aria-label="Open navigation menu">menu</button>
-          <span className="mdc-top-app-bar__title">Inicio</span>
-        </section>
-        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
-          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Favorite">favorite</button>
-          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Search">search</button>
-          <button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Options">more_vert</button>
-        </section>
-      </div>
-    </header>
-  )
+  let topappbar = new MDCTopAppBarBaseFoundation()
+
+  topappbar = (<div></div>)
 
   return topappbar;
 }
