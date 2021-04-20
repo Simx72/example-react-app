de763469f1ba9e04e5562ca75ce821439d9f8138
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 11:08:15 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,7 +1,7 @@
 import TopAppBar, { TopAppBarFixedAdjust, TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
 
-import '@material/react-material-icon/index.scss';
+import '@material/react-material-icon/dist/material-icon.css';
 import '@material/react-top-app-bar/index.scss';
 import '@material/react-list/index.scss';
 import '@material/react-drawer/index.scss';
@@ -13,7 +13,7 @@ const AppHeader = () => {
         <TopAppBarRow>
           <TopAppBarSection align='start'>
             <TopAppBarIcon navIcon tabIndex={0}>
-              <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')}/>
+              <MaterialIcon hasRipple={true} icon='menu' onClick={() => console.log('click')}/>
             </TopAppBarIcon>
             <TopAppBarTitle>Miami, FL</TopAppBarTitle>
           </TopAppBarSection>
@@ -21,7 +21,7 @@ const AppHeader = () => {
             <TopAppBarIcon actionItem tabIndex={0}>
               <MaterialIcon 
                 aria-label="print page" 
-                hasRipple 
+                hasRipple={true} 
                 icon='print' 
                 onClick={() => console.log('print')}
               />
