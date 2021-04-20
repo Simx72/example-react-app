ba82f11a53aa3ed068bcf899371dcd422a8eb713
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 17:20:38 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,8 +1,5 @@
-import TopAppBar, { TopAppBarFixedAdjust, TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
+import TopAppBar, { TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
-import { Component } from 'react';
-import Drawer, { DrawerAppContent, DrawerContent, DrawerHeader, DrawerTitle, DrawerSubtitle } from '@material/react-drawer';
-import List, { ListItem, ListItemText, ListItemGraphic } from '@material/react-list';
 
 import 'material-design-icons/iconfont/material-icons.css';
 import '@material/react-material-icon/dist/material-icon.css';
@@ -13,7 +10,7 @@ import './sass/material-icons.scss'
 import './sass/drawer.scss'
 
 
-const Navbar = () => {
+const AppHeader = () => {
   return (
     <TopAppBar>
       <TopAppBarRow>
@@ -31,74 +28,6 @@ const Navbar = () => {
       </TopAppBarRow>
     </TopAppBar>
   );
-}
-
-class AppHeader extends Component {
-  state = { selectedIndex: 0 };
-
-  componentDidMount() {
-    switch (window.location.pathname) {
-      case '/':
-        this.setState({
-          selectedIndex: 0
-        })
-        break;
-      case '/otro':
-        this.setState({
-          selectedIndex: 1
-        })
-        break;
-    
-      default:
-        break;
-    }
-  }
-
-  listEltClick(elt?: HTMLElement) {
-    if (typeof elt != 'undefined') {
-      let href = elt.getAttribute('href')
-      if (href != null) {
-        window.location.assign(href)
-      }
-    }
-  }
-
-  render() {
-    return (
-      <div className='drawer-container'>
-        <Drawer>
-          <DrawerHeader>
-            <DrawerTitle tag='h2'>
-              cualquier.correo@gmail.com
-            </DrawerTitle>
-            <DrawerSubtitle tag='em'>
-              @nombre_de_usuario
-            </DrawerSubtitle>
-          </DrawerHeader>
-
-          <DrawerContent>
-            <List singleSelection selectedIndex={this.state.selectedIndex}>
-              <ListItem href="/" onClick={(ev) => this.listEltClick(ev.target as HTMLElement)}>
-                <ListItemGraphic graphic={<MaterialIcon icon='home' />} />
-                <ListItemText primaryText="Inicio" />
-              </ListItem>
-              <ListItem href="/otro" onClick={(ev) => this.listEltClick(ev.target as HTMLElement)}>
-              <ListItemGraphic graphic={<MaterialIcon icon='favorite' />} />
-                Cualquier otra cosa
-              </ListItem>
-            </List>
-          </DrawerContent>
-        </Drawer>
-
-        <DrawerAppContent className='drawer-app-content'>
-          <Navbar />
-          <TopAppBarFixedAdjust>
-            Your inbox content
-          </TopAppBarFixedAdjust>
-        </DrawerAppContent>
-      </div>
-    );
-  }
 }
 
 export { AppHeader }
