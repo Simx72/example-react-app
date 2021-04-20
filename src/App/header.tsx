298556eae6264d919274c29095d90fe0363ae0e8
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 07:22:10 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -1,12 +1,42 @@
-import MDCTopAppBarBaseFoundation from "@material/top-app-bar/foundation";
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from '@material/react-top-app-bar';
+import MaterialIcon from '@material/react-material-icon';
 
 function AppHeader() {
 
-  let topappbar = new MDCTopAppBarBaseFoundation()
+  return (
+    <div>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection align='start'>
+            <TopAppBarIcon navIcon tabIndex={0}>
+              <MaterialIcon hasRipple icon='menu' onClick={() => console.log('click')} />
+            </TopAppBarIcon>
+            <TopAppBarTitle>Miami, FL</TopAppBarTitle>
+          </TopAppBarSection>
+          <TopAppBarSection align='end' role='toolbar'>
+            <TopAppBarIcon actionItem tabIndex={0}>
+              <MaterialIcon
+                aria-label="print page"
+                hasRipple
+                icon='print'
+                onClick={() => console.log('print')}
+              />
+            </TopAppBarIcon>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <TopAppBarFixedAdjust>
+        My exciting content!
+      </TopAppBarFixedAdjust>
+    </div>
+  )
 
-  let div = (<div></div>)
-
-  return div;
 }
 
 export { AppHeader }
