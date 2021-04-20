376cd50e11c6287d42e2e544d4808e0dc3b72a6e
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-20 06:59:52 -0500

    add to project

--- src/App/header.tsx
+++ src/App/header.tsx
@@ -16,30 +16,6 @@ function AppHeader() {
     </header>
   )
 
-  /* let drawer = (
-    <aside className="mdc-drawer">
-      <div className="mdc-drawer__content">
-        <nav className="mdc-list">
-          <a className="mdc-list-item mdc-list-item--activated" href="/" aria-current="page">
-            <span className="mdc-list-item__ripple"></span>
-            <i className="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>
-            <span className="mdc-list-item__text">Inbox</span>
-          </a>
-          <a className="mdc-list-item" href="item-2">
-            <span className="mdc-list-item__ripple"></span>
-            <i className="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>
-            <span className="mdc-list-item__text">Outgoing</span>
-          </a>
-          <a className="mdc-list-item" href="item-3">
-            <span className="mdc-list-item__ripple"></span>
-            <i className="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>
-            <span className="mdc-list-item__text">Drafts</span>
-          </a>
-        </nav>
-      </div>
-    </aside>
-  )*/
-
   return topappbar;
 }
 
