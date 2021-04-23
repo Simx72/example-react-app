2c0e1665f9c2370c521847ccd4b29a5c29fd9c93
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 17:14:19 -0500

    add to project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,10 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-/*
-test('renders learn react link', () => {
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('TopAppBar renders correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  document.getElementById('top-app-bar')
 });
-*/
+
 
 export {}
