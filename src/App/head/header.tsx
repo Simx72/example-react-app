cb9feb03645353ac8a846ce1f38749ed3a71728c
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 09:59:39 -0500

    add to project

--- src/App/head/header.tsx
+++ src/App/head/header.tsx
@@ -30,7 +30,6 @@ class AppHeader extends Component<AppHeaderProps, AppHeaderStates> {
   }
 
   onMenuClick(ev: ReactMouseEvent<HTMLElement, MouseEvent>) {
-    ev.preventDefault()
     this.setState({
       navBarMenuStates: {
         coordinates: {
