010f78d05beaa6bd5890c89d582f49468adbdfcd
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 18:03:02 -0500

    add to project

--- src/App/head/header.test.tsx
+++ src/App/head/header.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { AppHeader } from "./header";
 
 test('mdc-top-app-bar renders fine!', () => {
-  const res = render(<AppHeader onMenuClick={()=>{}} titulo="Hola" />)
+  const res = render(<AppHeader onMenuClick={()=>{}} titulo="Titulo de prueba" />)
   expect(res.getByText('Titulo de prueba')).toBeInTheDocument()
   expect(res.getByTestId('top-app-bar')).toHaveDisplayValue('render')
 })
