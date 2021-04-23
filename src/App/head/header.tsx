8af8102f40b0ea4f1a0c9f89f805e2f1f3e7ebca
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 07:51:37 -0500

    add to project

--- src/App/head/header.tsx
+++ src/App/head/header.tsx
@@ -1,22 +1,51 @@
 import TopAppBar, { TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
 import { NavBarMenu } from './menu-button';
+import { Component } from 'react';
 
-const AppHeader = (props: { titulo?: string, onMenuClick: () => void }) => {
-  return (
+interface AppHeaderProps {
+  titulo?: string,
+  onMenuClick: () => void
+}
+interface AppHeaderStates {
+  navBarMenuStates: {
+    coordinates: {
+      x: number;
+      y: number
+    }
+    open: boolean
+  }
+}
+
+class AppHeader extends Component<AppHeaderProps, AppHeaderStates> {
+  componentDidMount() {
+    this.setState({
+      navBarMenuStates: {
+        coordinates: {
+          x: 0,
+          y: 0
+        },
+        open: false
+      }
+    })
+  }
+  render = () => (
     <TopAppBar>
       <TopAppBarRow>
         <TopAppBarSection align='start'>
-          <TopAppBarIcon navIcon tabIndex={0} onClick={() => props.onMenuClick()}>
+          <TopAppBarIcon navIcon tabIndex={0} onClick={() => this.props.onMenuClick()}>
             <MaterialIcon hasRipple={true} icon='menu' />
           </TopAppBarIcon>
-          <TopAppBarTitle>{ props.titulo }</TopAppBarTitle>
+          <TopAppBarTitle>{this.props.titulo}</TopAppBarTitle>
         </TopAppBarSection>
         <TopAppBarSection align='end' role='toolbar'>
           <TopAppBarIcon actionItem tabIndex={0}>
             <MaterialIcon hasRipple={true} icon='settings' />
           </TopAppBarIcon>
-          <NavBarMenu x={0} y={0} />
+          <NavBarMenu
+            x={this.state.navBarMenuStates.coordinates.x}
+            y={this.state.navBarMenuStates.coordinates.y}
+          />
         </TopAppBarSection>
       </TopAppBarRow>
     </TopAppBar>
