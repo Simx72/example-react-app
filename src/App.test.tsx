023e58410a8ec69bc685e9ca30de022ff5d4e026
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 18:06:22 -0500

    add to project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,10 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
 test('still nothing', () => {
-  expect(0).toEqual(0)
+  const res = render(<App />)
+  expect(res.baseElement).toBeInTheDocument()
+  expect(res.getByTestId('app')).toHaveDisplayValue('Renders Good!')
 })
 
 export { };
