42b54c968c32c00c4b20d5870e26c692023c1ab3
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 07:41:54 -0500

    add to project

--- src/App/head/header.tsx
+++ src/App/head/header.tsx
@@ -1,5 +1,6 @@
 import TopAppBar, { TopAppBarIcon, TopAppBarRow, TopAppBarSection, TopAppBarTitle } from '@material/react-top-app-bar';
 import MaterialIcon from '@material/react-material-icon';
+import { NavBarMenu } from './menu-button';
 
 const AppHeader = (props: { titulo?: string, onMenuClick: () => void }) => {
   return (
@@ -12,9 +13,7 @@ const AppHeader = (props: { titulo?: string, onMenuClick: () => void }) => {
           <TopAppBarTitle>{ props.titulo }</TopAppBarTitle>
         </TopAppBarSection>
         <TopAppBarSection align='end' role='toolbar'>
-          <TopAppBarIcon actionItem tabIndex={0}>
-            <MaterialIcon hasRipple={true} icon='settings' />
-          </TopAppBarIcon>
+          <NavBarMenu x={0} y={0} />
         </TopAppBarSection>
       </TopAppBarRow>
     </TopAppBar>
