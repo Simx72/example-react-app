fa65d3f0a6a25f870a00cc66078b1c26a14ed74c
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-23 17:16:02 -0500

    add to project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { queryByTestId, render } from '@testing-library/react';
 import App from './App';
 
 test('TopAppBar renders correctly', () => {
   render(<App />);
-  document.getElementById('top-app-bar')
+  expect(queryByTestId('top-app-bar')).toBeInTheDocument()
 });
 
 
