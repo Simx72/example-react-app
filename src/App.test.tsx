87618f194527641c9520ed3cc2ad2160139e50bd
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 07:46:02 -0500

    add to project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 jest.mock('react-chartjs-2', () => ({
-  Bar: () => null, // add any additional chart types here
+  Bar: () => null,
   Line: () => null
 }));
 
