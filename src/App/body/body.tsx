744732a01cb43b403b548ddb3c0bca683fe0db40
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 09:46:04 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,34 +1,39 @@
 import Card, { CardMedia, CardPrimaryContent } from '@material/react-card';
+import { Row, Col } from 'react-grid-system';
 import * as Chart from 'react-chartjs-2';
 
 const AppBody = () => (
-  <div id="App-body" className="l-wrap">
-    <Card>
-      <CardPrimaryContent>
-        <CardMedia square >
-          <Chart.Bar
-            type="bar"
-            data={{
-              labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-              datasets: [
-                {
-                  label: "Population (millions)",
-                  backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                  data: [2478, 5267, 734, 784, 433]
-                }
-              ]
-            }}
-            options={{
-              legend: { display: false },
-              title: {
-                display: true,
-                text: 'Predicted world population (millions) in 2050'
-              }
-            }}
-          />
-        </CardMedia>
-      </CardPrimaryContent>
-    </Card>
+  <div id="App-body">
+    <Row>
+      <Col>
+        <Card>
+          <CardPrimaryContent>
+            <CardMedia square >
+              <Chart.Bar
+                type="bar"
+                data={{
+                  labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+                  datasets: [
+                    {
+                      label: "Population (millions)",
+                      backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                      data: [2478, 5267, 734, 784, 433]
+                    }
+                  ]
+                }}
+                options={{
+                  legend: { display: false },
+                  title: {
+                    display: true,
+                    text: 'Predicted world population (millions) in 2050'
+                  }
+                }}
+              />
+            </CardMedia>
+          </CardPrimaryContent>
+        </Card>
+      </Col>
+    </Row>
   </div>
 )
 
