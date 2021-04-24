99129dc47103e9c7ab143af588d4e1ff5aa67ceb
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 13:36:57 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -5,24 +5,9 @@ const AppBody = () => (
   <div id="App-body">
     <Grid>
         <Row>
-          <Cell columns={4}>Tennis</Cell>
-          <Cell columns={4}>Cricket</Cell>
-          <Cell columns={4}>StarCraft</Cell>
-        </Row>
-        <Row>
-          <Cell desktopColumns={4} order={2} phoneColumns={4} tabletColumns={4}>Tennis</Cell>
-          <Cell desktopColumns={4} order={3} phoneColumns={4} tabletColumns={4}>Cricket</Cell>
-          <Cell desktopColumns={4} order={1} phoneColumns={4} tabletColumns={4}>StarCraft</Cell>
-        </Row>
-        <Row>
-          <Cell columns={4}>
-            <Row>
-              <Cell desktopColumns={8} phoneColumns={2} tabletColumns={5}>Tennis</Cell>
-              <Cell desktopColumns={4} phoneColumns={2} tabletColumns={3}>Cricket</Cell>
-            </Row>
+        <Cell columns={6}>
+          <Chart1 />
           </Cell>
-          <Cell columns={4}> - </Cell>
-          <Cell columns={4}> - </Cell>
         </Row>
       </Grid>
   </div>
