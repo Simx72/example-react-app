d0e0d42990e3276f2eeb85c9761251cc0c41e903
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 13:33:12 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,13 +1,30 @@
-import { Row, Col } from 'react-grid-system';
+import { Grid, Row, Cell } from '@material/react-layout-grid';
 import { Chart1 } from './chart';
 
 const AppBody = () => (
   <div id="App-body">
-    <Row>
-      <Col lg={6}>
-        <Chart1 />
-      </Col>
-    </Row>
+    <Grid>
+        <Row>
+          <Cell columns={4}>Tennis</Cell>
+          <Cell columns={4}>Cricket</Cell>
+          <Cell columns={4}>StarCraft</Cell>
+        </Row>
+        <Row>
+          <Cell desktopColumns={4} order={2} phoneColumns={4} tabletColumns={4}>Tennis</Cell>
+          <Cell desktopColumns={4} order={3} phoneColumns={4} tabletColumns={4}>Cricket</Cell>
+          <Cell desktopColumns={4} order={1} phoneColumns={4} tabletColumns={4}>StarCraft</Cell>
+        </Row>
+        <Row>
+          <Cell columns={4}>
+            <Row>
+              <Cell desktopColumns={8} phoneColumns={2} tabletColumns={5}>Tennis</Cell>
+              <Cell desktopColumns={4} phoneColumns={2} tabletColumns={3}>Cricket</Cell>
+            </Row>
+          </Cell>
+          <Cell columns={4}> - </Cell>
+          <Cell columns={4}> - </Cell>
+        </Row>
+      </Grid>
   </div>
 )
 
