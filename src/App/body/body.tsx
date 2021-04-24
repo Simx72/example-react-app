b8c68cd6631babcf430ae07328c849b17fda1961
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 13:30:00 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,11 +1,11 @@
 import { Row, Col } from 'react-grid-system';
-import { Card1 } from './chart';
+import { Chart1 } from './chart';
 
 const AppBody = () => (
   <div id="App-body">
     <Row>
       <Col lg={6}>
-        <Card1 />
+        <Chart1 />
       </Col>
     </Row>
   </div>
