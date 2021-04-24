e9ae2a207cdadeed390fd1b27bf52f3d473dcc82
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 12:50:13 -0500

    add to project

--- src/App/head/header.tsx
+++ src/App/head/header.tsx
@@ -38,7 +38,7 @@ class AppHeader extends Component<AppHeaderProps, AppHeaderStates> {
               },
               {
                 text: 'Log Out',
-                icon: 'home'
+                icon: 'exit'
               },
             ]} />
         </TopAppBarSection>
