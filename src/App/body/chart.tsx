fdcdf03c116696f98a1f97dff6626aa5911c1f51
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 15:54:25 -0500

    add to project

--- src/App/body/chart.tsx
+++ src/App/body/chart.tsx
@@ -77,28 +77,28 @@ const Chart2 = () => {
             data={{
               labels: stockMarketData.map(val => val.date.getFullYear()+' - '+val.date.getMonth()),
               datasets: [{
-                data: stockMarketData.map(val => val.),
-                label: "Africa",
+                data: stockMarketData.map(val => val.open),
+                label: "Open",
                 borderColor: "#3e95cd",
                 fill: false
               }, {
-                data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-                label: "Asia",
+                data: stockMarketData.map(val => val.close),
+                label: "Close",
                 borderColor: "#8e5ea2",
                 fill: false
               }, {
-                data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-                label: "Europe",
+                data: stockMarketData.map(val => val.high),
+                label: "High",
                 borderColor: "#3cba9f",
                 fill: false
               }, {
-                data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-                label: "Latin America",
+                data: stockMarketData.map(val => val.low),
+                label: "Low",
                 borderColor: "#e8c3b9",
                 fill: false
               }, {
-                data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-                label: "North America",
+                data: stockMarketData.map(val => val.volume),
+                label: "Volume",
                 borderColor: "#c45850",
                 fill: false
               }
