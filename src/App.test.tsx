b6e33e91b14d766f99a4070305ceb5d716956dd3
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 07:44:59 -0500

    add to project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,10 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test('App renders correctly', () => {
-  jest.mock('react-chartjs-2', () => 'Chart')
+  jest.mock('react-chartjs-2', () => ({
+    Doughnut: () => null,
+    Bar: () => null
+  }))
   const res = render(<App />)
   expect(res.baseElement).toBeInTheDocument()
   expect(res.getByTestId('app')).toHaveDisplayValue('Renders Good!')
