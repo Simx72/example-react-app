a2ff73f6d8934b5f60a3966ba468a6a75d152200
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 11:46:17 -0500

    add to project

--- src/App/head/menu-button.tsx
+++ src/App/head/menu-button.tsx
@@ -3,16 +3,21 @@ import Menu, { MenuList, MenuListItem, MenuListItemGraphic, MenuListItemText } f
 import { TopAppBarIcon } from "@material/react-top-app-bar";
 import { Component, MouseEvent as ReactMouseEvent } from 'react';
 
-let MenuOption = (props: { text: string, icon: string, key: string | number }) => (
-  <MenuListItem key={props.key} style={{ display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-    <MenuListItemText primaryText={props.text} />
-    <MenuListItemGraphic graphic={<MaterialIcon icon={props.icon} />} />
-  </MenuListItem>
+interface MenuOptionInterface {
+  text: string
+  icon: string
+}
+
+let MenuOption = (props: MenuOptionInterface): MenuOptionInterface => (
+  {
+    text: props.text,
+    icon: props.icon
+  }
 )
 
 interface ButtonWithMenuProps {
   ButtonClass: (typeof TopAppBarIcon)
-  children: typeof MenuOption[]
+  children: MenuOptionInterface[]
   onSelect?: (index: number, item: Element) => void
 }
 interface ButtonWithMenuStates {
@@ -60,11 +65,18 @@ class ButtonWithMenu extends Component<ButtonWithMenuProps, ButtonWithMenuStates
           onSelected={this.onSelected}
           onClose={this.onClose}
         >
-          <MenuList style={{ overflow: 'hidden' }} children={this.props.children} />
+          <MenuList style={{ overflow: 'hidden' }}>
+            {this.props.children.map((option, index) => (
+              <MenuListItem key={index} style={{ display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <MenuListItemText primaryText={option.text} />
+                <MenuListItemGraphic graphic={<MaterialIcon icon={option.icon} />} />
+              </MenuListItem>
+            ))}
+          </MenuList>
         </Menu>
       </>
     )
   }
 }
 
-export {MenuOption, ButtonWithMenu}
+export { MenuOption, ButtonWithMenu }
