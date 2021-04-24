136187294d033eee458232b91a10a479d5cadf13
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 13:29:40 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,43 +1,11 @@
-import Card, { CardPrimaryContent, CardActions, CardActionIcons } from '@material/react-card';
 import { Row, Col } from 'react-grid-system';
-import * as Chart from 'react-chartjs-2';
-import MaterialIcon from '@material/react-material-icon';
+import { Card1 } from './chart';
 
 const AppBody = () => (
   <div id="App-body">
     <Row>
       <Col lg={6}>
-        <Card>
-          <CardPrimaryContent style={{ padding: '1rem' }} >
-            <Chart.Bar
-              type="bar"
-              data={{
-                labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-                datasets: [
-                  {
-                    label: "Population (millions)",
-                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                    data: [2478, 5267, 734, 784, 433]
-                  }
-                ]
-              }}
-              options={{
-                legend: { display: false },
-                title: {
-                  display: true,
-                  text: 'Predicted world population (millions) in 2050'
-                }
-              }}
-            />
-          </CardPrimaryContent>
-          <CardActions>
-            <CardActionIcons>
-              <MaterialIcon icon='more_vert' />
-              <MaterialIcon icon='more' />
-              <MaterialIcon icon='favorite' />
-            </CardActionIcons>
-          </CardActions>
-        </Card>
+        <Card1 />
       </Col>
     </Row>
   </div>
