1357f20fa7c230934953b1aee339756e9a433270
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-24 14:08:13 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,17 +1,24 @@
 import { Grid, Row, Cell } from '@material/react-layout-grid';
+import { useGet } from 'restful-react';
 import { Chart1 } from './chart';
 
-const AppBody = () => (
-  <div id="App-body">
-    <Grid>
-      <Row>
-        <Cell columns={6}>
-          <Chart1 />
-        </Cell>
-      </Row>
-    </Grid>
-  </div>
-)
+const AppBody = () => {
+  
+  let data = useGet('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo')
+  
+  return (
+    <div id="App-body">
+      <Grid>
+        <Row>
+          <Cell columns={6}>
+            <Chart1 />
+          </Cell>
+        </Row>
+      </Grid>
+      <div>{data}</div>
+    </div>
+  )
+}
 
 
 export { AppBody }
