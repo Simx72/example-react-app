e99a921da7b62a639613c9ffebacbbda3ff52d57
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 14:54:57 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -5,7 +5,7 @@ let alignCenter: { textAlign: 'center' } = { textAlign: 'center' }
 const Home = () => (
   <div className='color-complementary-900'>
     <Txt.Headline1 style={alignCenter} >Home</Txt.Headline1>
-    <Txt.Subtitle1 style={alignCenter} >This is the main page of the project</Txt.Subtitle1>
+    <Txt.Headline2 style={alignCenter} >This is the main page of the project</Txt.Headline2>
   </div>
 )
 
