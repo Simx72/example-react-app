95daca5c57fc41458111f6770ae1968f52c7ed9e
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 17:33:11 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -36,7 +36,7 @@ const Home = () => {
                 {val}
                 <CardActions>
                   <CardActionIcons>
-                    {<MDI icon='favorite' cardAction />}
+                    {<MDI icon='3d' cardAction />}
                   </CardActionIcons>
                 </CardActions>
               </Card>
