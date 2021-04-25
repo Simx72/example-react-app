88158ac55a1036849353c8ca49c02d61d10729b7
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 10:59:04 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -2,32 +2,28 @@ import { Chart1, Chart2 } from './chart';
 import { MaterialIconTwoTone as MDI } from '../material-icon';
 
 const AppBody = (props: { index: number }) => {
+  let elt: JSX.Element;
+  switch (props.index) {
+    case 1:
+      elt = (<Chart2 />)
+      break
+    case 2:
+      elt = (<Chart1 />)
+      break
+    default:
+      elt = (
+        <div>
+          <MDI icon='home' />
+          <span>Inicio</span>
+          <MDI icon='home' />
+        </div>
+      )
+      break
+  }
+
   return (
     <div id="App-body">
-      {(() => {
-        switch (props.index) {
-          case 0:
-            return (
-              <div>
-                <MDI icon='home' />
-                <span>Inicio</span>
-                <MDI icon='home' />
-              </div>
-            )
-          case 1:
-            return (<Chart2 />)
-          case 2:
-            return (<Chart1 />)
-          default:
-            return (
-              <div>
-                <MDI icon='home' />
-                <span>Inicio</span>
-                <MDI icon='home' />
-              </div>
-            )
-        }
-      })}
+      {elt}
     </div>
   )
 }
