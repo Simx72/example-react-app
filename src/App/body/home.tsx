0ab1ff6fb9ee2a415c4e538585d8474ff07cdf7a
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 11:23:02 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -1,1 +1,11 @@
-export {}
+import { MaterialIconTwoTone as MDI } from '../material-icon';
+
+const Home = () => (
+  <div>
+    <MDI icon='home' />
+    <span>Inicio</span>
+    <MDI icon='home' />
+  </div>
+)
+
+export { Home }
