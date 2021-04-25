71923e5af931e8106eea9ab6d041057b41b9d751
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 17:17:17 -0500

    add to project

--- src/App/material-icon.tsx
+++ src/App/material-icon.tsx
@@ -1,23 +1,23 @@
-const DefaultIcon = (props: { icon: string, className: string, hasRipple: boolean }) => {
+const DefaultIcon = (props: { icon: string, className: string, hasRipple?: boolean }) => {
   if (props.hasRipple) {
     return (<span className={'material-icons--ripple-surface ' + props.className} >{props.icon}</span>)
   } else {
     return (<span className={props.className} >{props.icon}</span>)
   }
 }
-const MaterialIcon = (props: { icon: string, hasRipple: boolean = false }) => (
+const MaterialIcon = (props: { icon: string, hasRipple?: boolean }) => (
   <DefaultIcon icon={props.icon} hasRipple={props.hasRipple} className='material-icons' />
 )
-const MaterialIconOutlined = (props: { icon: string, hasRipple: boolean = false }) => (
+const MaterialIconOutlined = (props: { icon: string, hasRipple?: boolean }) => (
   <DefaultIcon icon={props.icon} hasRipple={props.hasRipple} className='material-icons-outlined' />
 )
-const MaterialIconRound = (props: { icon: string, hasRipple: boolean = false }) => (
+const MaterialIconRound = (props: { icon: string, hasRipple?: boolean }) => (
   <DefaultIcon icon={props.icon} hasRipple={props.hasRipple} className='material-icons-round' />
 )
-const MaterialIconSharp = (props: { icon: string, hasRipple: boolean = false }) => (
+const MaterialIconSharp = (props: { icon: string, hasRipple?: boolean }) => (
   <DefaultIcon icon={props.icon} hasRipple={props.hasRipple} className='material-icons-sharp' />
 )
-const MaterialIconTwoTone = (props: { icon: string, hasRipple: boolean = false }) => (
+const MaterialIconTwoTone = (props: { icon: string, hasRipple?: boolean }) => (
   <DefaultIcon icon={props.icon} hasRipple={props.hasRipple} className='material-icons-two-tone' />
 )
 
