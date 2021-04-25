1fb9ec1317a70a83c746e48608564b3c3fd53852
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 14:49:40 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -1,9 +1,11 @@
 import * as Txt from '@material/react-typography'
 
+let alignCenter: { textAlign: 'center' } = { textAlign: 'center' }
+
 const Home = () => (
   <div className='color-complementary-900'>
-    <Txt.Headline1 style={{ textAlign: 'center' }}>Home</Txt.Headline1>
-    <Txt.Subtitle1>This is the main page of the project</Txt.Subtitle1>
+    <Txt.Headline1 style={alignCenter} >Home</Txt.Headline1>
+    <Txt.Subtitle1 style={alignCenter} >This is the main page of the project</Txt.Subtitle1>
   </div>
 )
 
