761399d1d0944ba5f04178d892421040778136b7
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 11:33:53 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -1,7 +1,7 @@
 import { MaterialIconTwoTone as MDI } from '../material-icon';
 
 const Home = () => (
-  <div style={{ color: '--color-complementary-900', textAlign: 'center', width: '100%' }} >
+  <div className='color-complementary-500' style={{ textAlign: 'center', width: '100%' }} >
     <MDI icon='home' />
     <span>Inicio</span>
     <MDI icon='home' />
