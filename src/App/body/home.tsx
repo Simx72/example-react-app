fa45d472e69f6d2085bac5bafcf2577ef1d929d6
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 18:31:41 -0500

    add to project

--- src/App/body/home.tsx
+++ src/App/body/home.tsx
@@ -2,7 +2,6 @@ import { Cell, Row, Grid } from '@material/react-layout-grid'
 import Card, { CardActions, CardActionIcons } from '@material/react-card';
 import { EightColumn, FourColumn, TwelveColumn } from '@material/react-layout-grid/dist/Cell'
 import * as Txt from '@material/react-typography'
-import { MaterialIconRound as MDI } from '../material-icon';
 import { Favorite } from '../functions/favorite';
 
 let alignCenter: { textAlign: 'center' } = { textAlign: 'center' }
