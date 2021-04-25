4103203caacee654cfaa39e3413d0f0e67af6889
Simx72 <53983258+Simx72@users.noreply.github.com>
2021-04-25 10:34:56 -0500

    add to project

--- src/App/body/body.tsx
+++ src/App/body/body.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Chart2 } from './chart';
+import { Chart1, Chart2 } from './chart';
 
 const AppBody = () => {
   return (
     <div id="App-body">
       <BrowserRouter>
         <Switch>
+          <Route path="/population">
+            <Chart1 />
+          </Route>
           <Route path="/ibm-stock-market">
             <Chart2 />
           </Route>
